27c7fc7dd73a7c7ec304aa6f98c18e6fcf3ed750
Chantal Goethals <chantalgoethals@gmail.com>
2022-10-16 23:04:36 -0600

    Feat: build out icons and responsive menu

---
+++ src/routes/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Header/>
+        <h2>Home</h2>  
+    </div>
+  )
+}
+
+export default Home
