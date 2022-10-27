d8416e73be5fe3a0b7b74ad5440b6d9f85cd4e45
Chantal Goethals <chantalgoethals@gmail.com>
2022-10-27 13:05:50 -0600

    Feat: add names to form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,13 +8,13 @@ const Form = () => {
       <div className='form'>
         <form>
           <label>Your Name</label>
-            <input type="text"></input>
+            <input type="text" name="user_name"></input>
           <label>Email</label>
-            <input type="email"></input>
+            <input type="email" name="user_email"></input>
           <label>Subject</label>
-            <input type="text"></input>
+            <input type="text" name="subject"></input>
           <label>Message</label>
-            <textarea rows='6' placeholder='Type your message here' type="text"></textarea>
+            <textarea rows='6' placeholder='Type your message here' type="text" name="message"></textarea>
           <button className='btn'>Submit</button>
           </form>
       </div>
