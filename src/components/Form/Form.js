9d26ba5f24d68253d4366a11743df08e6e5c55c8
Chantal Goethals <chantalgoethals@gmail.com>
2022-10-27 13:13:44 -0600

    Feat: add email formatting

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser';
 import './Form.css'
 
 
 
 const Form = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_qg4fide', 'template_u9ktssv', form.current, '6zrSqK7yVZ2g5Sy0O')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset
+  };
   return (
       <div className='form'>
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
           <label>Your Name</label>
             <input type="text" name="user_name"></input>
           <label>Email</label>
