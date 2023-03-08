a3fa82971ada88ccd4c3e440288e33593e3a69a1
Chantal Goethals <chantalgoethals@gmail.com>
2023-03-08 15:46:41 -0700

    Feat: update project linkes

--- src/components/WorkCardData/WorkCardData.js
+++ src/components/WorkCardData/WorkCardData.js
@@ -49,6 +49,13 @@
 
 const ProjectCardData = [
   {
+    imgsrc: "https://media.giphy.com/media/y4iIyVWg0sGgamV6zk/giphy.gif",
+    title: "Concert Crew",
+    text: "This application helps groups of friends connect over upcoming concerts. It was created with both a front-end and back-end team of Turing students utilizing React, Ruby on Rails, and GraphQL implementing consumption of REST API with upcoming show data. A user is able to log in at the home view. The user will be redirected with React Router to view up upcoming shows. The user is able to select an upcoming show and view show details with map location/navigation. The user is able to navigate to and view upcoming friend's shows and choose to attend each show. The user is able to search for upcoming shows and choose to attend.",
+    view: "https://concert-crew.vercel.app/",
+    source: "https://github.com/concert-crew/client",
+  },
+  {
     imgsrc: "https://media.giphy.com/media/xBCLovu3spMFYilkwB/giphy.gif",
     title: "Angular Distance",
     text: "An app that guides users to a luxury space flight reservation experience. This application was built with JavaScript in the React framework. Implemented REST API consumption with planet data and tested with Cypress. The user will visit the landing page and navigate through a journey of user stories displayed with a carousel. The user is able to navigate to a reservations page with React-Router and view planet-shaped buttons to select a planet destination. The user is also able to select a moon that corresponds to the planet. Once, the user has selected a date and destination the user is then re-directed to a page to view a list of their reservations.",
@@ -59,7 +66,7 @@ const ProjectCardData = [
     imgsrc: "https://media.giphy.com/media/PQoQebukk8YXsL93q0/giphy.gif",
     title: "Rancid Tomatillos",
     text: "A JavaScript App built in React that consumes a REST API with movie data. The landing page allows a user to view a random featured movie and smaller movie cards organized into accessible carousels. The user is able to select a movie and navigate with React Router to a single movie view. The single movie view displays details and a movie trailer. If there is more than one movie trailer, the user is able to swipe through multiple trailers with their mouse or keyboard.",
-    view: "https://rancid-tomatillos-gold.vercel.app/",
+    view: "https://rancid-tomatillos-seven.vercel.app/",
     source: "https://github.com/GOECHA/goecha-rancid-tomatillos",
   },
   {
@@ -69,13 +76,6 @@ const ProjectCardData = [
     text: "A JavaScript application built in React-Native and consumption of a REST API of Compliments and Mantras built with Express. This is an application that allows a user to view a random mantra and choose to save the mantra to a list within the application for a daily dose of positive vibes. Icons designed with Adobe Illustrator.",
     view: "https://nymin.vercel.app/",
     source: "https://github.com/GOECHA/nymin",
-  },
-  {
-    imgsrc: "https://media.giphy.com/media/y4iIyVWg0sGgamV6zk/giphy.gif",
-    title: "Concert Crew",
-    text: "This application helps groups of friends connect over upcoming concerts. It was created with both a front-end and back-end team of Turing students utilizing React, Ruby on Rails, and GraphQL implementing consumption of REST API with upcoming show data. A user is able to log in at the home view. The user will be redirected with React Router to view up upcoming shows. The user is able to select an upcoming show and view show details with map location/navigation. The user is able to navigate to and view upcoming friend's shows and choose to attend each show. The user is able to search for upcoming shows and choose to attend.",
-    view: "https://concert-crew.herokuapp.com/",
-    source: "https://github.com/concert-crew/client",
   },
   {
     imgsrc: "https://media.giphy.com/media/LyAnK9juVlQ0XNHBSX/giphy.gif",
